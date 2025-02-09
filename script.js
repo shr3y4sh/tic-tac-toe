2249a81a72f3436992f496fd4b67ee83e9df5ba3
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2025-02-09 13:25:25 +0530

    make logic, init objects and functions
    three major functions: player factory, gameBoard, controller
    One IIFE which start the game
    most variables are private, only methods are returned (mostly)

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function makePlayer(name, tacChoice) {
+function makePlayer(name, tac) {
+  // Player factory function, returns name and tacChoice.
+  // Name is not required for now, but we will see what happens
+  const tacChoice = () => tac;
+
   return {
     name,
     tacChoice,
@@ -6,8 +10,34 @@ function makePlayer(name, tacChoice) {
 }
 
 function GameBoard(player1, player2) {
-    const grid = [];
+  const grid = [];
+  const row = 3;
+  const col = 3;
+  // Grid of 3x3 with names 0-2
+  // grid[row][col]
+
+  const tac = {
+    X: "X",
+    O: "O",
+  };
+
+  for (let i = 0; i < row; i++) {
+    grid[i] = [];
+    for (let j = 0; j < col; j++) {
+      grid[i].push(0);
+    }
+  }
 }
 
+function Controller(grid, playerX, playerY) {
+  const gridUpdate = function () {};
 
+  const playerTurn = function () {};
+}
 
+const start = (function () {
+  const playerX = makePlayer("Shreyash", "X");
+  const playerY = makePlayer("Ishank", "O");
+
+  const board = GameBoard(playerX, playerY);
+})();
