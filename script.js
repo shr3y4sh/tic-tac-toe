2831a4a55378ce02f1561d2c3228d1a48d6e5639
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2025-02-18 16:15:09 +0530

    click button display board
    click individual cells input the X mark
    make two divs showing one and two player games
    click start button, show empty board.
    fix footer at bottom

--- script.js
+++ script.js
@@ -187,3 +187,51 @@ const hero = (function () {
 
   return master;
 })();
+
+function gameBoardDisplay() {
+  const gameGridHtml = `<div class="cell" id="1grid"></div>
+      <div class="cell" id="2grid"></div>
+      <div class="cell" id="3grid"></div>
+      <div class="cell" id="4grid"></div>
+      <div class="cell" id="5grid"></div>
+      <div class="cell" id="6grid"></div>
+      <div class="cell" id="7grid"></div>
+      <div class="cell" id="8grid"></div>
+      <div class="cell" id="9grid"></div>
+  `;
+  const gameBoard = document.createElement("div");
+  gameBoard.classList.add("game-board");
+
+  const body = document.querySelector("body");
+  const footer = document.querySelector("footer");
+  gameBoard.innerHTML = gameGridHtml;
+  body.insertBefore(gameBoard, footer);
+}
+
+// gameBoardDisplay();
+
+const clickToStart = (function () {
+  const emptyDiv = document.querySelector(".empty");
+
+  const startButton = document.querySelector("#start");
+
+  startButton.addEventListener("click", function () {
+    emptyDiv.remove();
+    gameBoardDisplay();
+    gameStart();
+  });
+})();
+
+const X_image = `<img src="assets/images/cancel.png"></div>`;
+const O_image = `<img src="assets/images/o.png"></div>`;
+
+function gameStart() {
+  const cells = document.querySelectorAll(".cell");
+
+  cells.forEach(element => {
+    element.addEventListener("click", function () {
+      console.log(element.getAttribute("id"))
+      element.innerHTML = X_image;
+    })
+  });
+}
